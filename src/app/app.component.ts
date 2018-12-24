29ce19e81a5b49c306a7e0236d01fa74730a5ce6
Marina Sirotkina <chmv7419@gmail.com>
2018-12-24 19:57:31 +0200

    TD forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-course-forms';
+
+  @ViewChild('form') form: NgForm;
+
+  answers = [{
+    type: 'yes',
+    text: 'да'
+  }, {
+    type: 'no',
+    text: 'нет'
+  }];
+
+  defaultAnswer = 'no';
+  defaultCountry = 'ua';
+  formData = {};
+  isSubmited = false;
+
+  addRandEmail() {
+    const randEmail = 'test@mail.me';
+    // this.form.setValue({
+    //   user: {
+    //     pass: '',
+    //     email: randEmail
+    //   },
+    //   country: '',
+    //   answer: ''
+    // });
+    this.form.form.patchValue({user: {email: randEmail}});
+  }
+
+  submitForm() {
+    this.isSubmited = true;
+    this.formData = this.form.value;
+    this.form.reset();
+  }
 }
